f623027161ef2198bf3869839420020224e254eb
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-31 21:45:14 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(array)
+  modified_array = []
+  i = 0
+  if block_given?
+    while i < array.length
+      modified_array << yield(array[i])
+      i += 1
+    end
+  else
+    puts "#{i}"
+  end
+  modified_array
+end
